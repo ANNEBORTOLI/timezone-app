db1691e1e22b95057fd18665d6685046dcfb8aab
Araceli Gallardo <acgallardoinchaurrondo@gmail.com>
2023-06-06 13:55:18 -0700

    controller

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,2 +1,26 @@
 class ConnectionsController < ApplicationController
+  def new
+    @connection = Connection.new
+  end
+
+  def create
+    @connection = Connection.new(connection_params)
+    if @connection.save
+      redirect_to @connection
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @connection = Connection.find(params[:id])
+    @connection.destroy
+    redirect_to @user
+  end
+
+  private
+
+  def connection_params
+    params.require(:connection).permit(:user_id, :group_id)
+  end
 end
