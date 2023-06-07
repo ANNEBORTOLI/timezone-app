96853cc38eb4831fdaa9f67a63087fd732441ee7
Anne Bortoli <annebortoli@gmail.com>
2023-06-07 18:21:17 -0300

    add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-
+  # root to: "pages#home"
+  root to: "users#show"
   resources :users, only: %i[index show]
   resources :connections, only: %i[new create destroy]
   resources :groups
