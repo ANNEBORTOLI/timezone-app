29d66d895994c22955a1990c6ec900088c2e6470
Anne Bortoli <annebortoli@gmail.com>
2023-06-08 17:57:30 -0300

    :construction: timezone logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,23 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, :last_name, :address, presence: true
   validates :phone, length: { maximum: 15 }
-  # validates :latitude, :longitude, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  before_save :set_timezone
 
-  # timezone = Timezone.lookup(-34.92771808058, 138.477041423321)
-  # #<Timezone::Zone name: "Australia/Adelaide">
+  def set_timezone
+    # timezone = Timezone.lookup(-34.92771808058, 138.477041423321)
+    # #<Timezone::Zone name: "Australia/Adelaide">
+    timezone = Timezone.lookup(latitude, longitude)
+    raise
+    # timezone.name
+    # #"Australia/Adelaide"
 
-  # timezone.name
-  # #"Australia/Adelaide"
-
-  # timezone.utc_to_local(Time.now)
-  # #2011-02-12 12:02:13 UTC
+    # timezone.utc_to_local(Time.now)
+    # #2011-02-12 12:02:13 UTC
+  end
 
   def all_contacts
     connections.map(&:contact) # connections.map { |connection| connection.contact }
