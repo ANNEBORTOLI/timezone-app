982543aeeddc5c13a2cf78b733083c3ec77d763d
bfazz <brunofazz80@gmail.com>
2023-06-08 16:34:36 -0300

    Edit Groups done

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -5,6 +5,17 @@ import TomSelect from "tom-select"
 export default class extends Controller {
   connect() {
     var settings = {}
-    new TomSelect( this.element, settings)
+    new TomSelect( this.element, {
+      plugins: {
+        remove_button:{
+          title:'Remove this item',
+        }
+      },
+      persist: false,
+      create: true,
+      onDelete: function(values) {
+        return confirm(values.length > 1 ? 'Are you sure you want to remove these ' + values.length + ' items?' : 'Are you sure you want to remove "' + values[0] + '"?');
+      }
+    });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show]
   resources :connections, only: %i[new create destroy]
   resources :groups
+  get 'test', to: 'pages#test'
 end
