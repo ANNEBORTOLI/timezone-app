cc2649d9073d00c516a9bbb0237f60fac1d6968c
Anne Bortoli <annebortoli@gmail.com>
2023-06-15 16:32:39 -0300

    config ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActionController::Redirecting::UnsafeRedirectError do
+    redirect_to root_url, allow_other_host: true
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :city, :country, :working_hour_start, :working_hour_end, :timezone, :phone, :latitude, :longitude ])
